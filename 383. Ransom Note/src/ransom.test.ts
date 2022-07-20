2084c2ab8fd04894facc1f10285702739e21db8f
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2022-07-19 18:16:31 -0700

    Solution 383: Added tests for examples 2 and 3.

--- 383. Ransom Note/src/ransom.test.ts
+++ 383. Ransom Note/src/ransom.test.ts
@@ -3,3 +3,11 @@ import canConstruct from './ransom'
 test('ransomNote = "a", magazine = "b" => false', () => {
   expect( canConstruct('a', 'b') ).toBe( false );
 });
+
+test('ransomNote = "aa", magazine = "ab" => false', () => {
+  expect( canConstruct('aa', 'ab') ).toBe( false );
+});
+
+test('ransomNote = "aa", magazine = "aab" => true', () => {
+  expect( canConstruct('aa', 'aab') ).toBe( true );
+});
--- 383. Ransom Note/src/ransom.ts
+++ 383. Ransom Note/src/ransom.ts
@@ -1,5 +1,18 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
-  return false;
+  let bucket: string[] = magazine.split('');
+  let result = true;
+
+  for ( const c of ransomNote.split('') ) {
+    const i = bucket.indexOf(c);
+    if ( i > -1 ) {
+      bucket.splice(i, 1)
+    } else {
+      result = false;
+      break;
+    }
+  }
+
+  return result;
 };
 
 export default canConstruct;
