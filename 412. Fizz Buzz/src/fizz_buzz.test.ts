8b85b416c63cfa72f79269e2d6ac3e23d73c12c4
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2022-07-21 02:22:06 -0700

    Solution 412: Added tests for examples 2 and 3.

--- 412. Fizz Buzz/src/fizz_buzz.test.ts
+++ 412. Fizz Buzz/src/fizz_buzz.test.ts
@@ -3,3 +3,11 @@ import fizzBuzz from './fizz_buzz'
 test('n = 3, ["1","2","Fizz"]', () => {
   expect( fizzBuzz(3) ).toEqual( expect.arrayContaining(["1","2","Fizz"]) );
 });
+
+test('n = 5, ["1","2","Fizz","4","Buzz"]', () => {
+  expect( fizzBuzz(5) ).toEqual( expect.arrayContaining(["1","2","Fizz","4","Buzz"]) );
+});
+
+test('n = 15, ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]', () => {
+  expect( fizzBuzz(15) ).toEqual( expect.arrayContaining(["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]) );
+});
--- 412. Fizz Buzz/src/fizz_buzz.ts
+++ 412. Fizz Buzz/src/fizz_buzz.ts
@@ -1,6 +1,21 @@
 function fizzBuzz(n: number): string[] {
+  let result = [];
+  let i = 1;
 
-  return ["1", "2", "Fizz"];
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      result[i] = "FizzBuzz";
+    } else if (i % 3 === 0) {
+      result[i] = "Fizz";
+    } else if (i % 5 === 0) {
+      result[i] = "Buzz";
+    } else {
+      result[i] = i.toString();
+    }
+  }
+
+  result.shift();
+  return result;
 };
 
 export default fizzBuzz;
