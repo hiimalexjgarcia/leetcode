48fc86ba8f229797340043910d0f8f1dfa7365aa
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2022-07-24 17:53:53 -0700

    Solution 1337: Added test for example 2.

--- 1337. The K Weakest Rows in a Matrix/src/k_weakest_rows.ts
+++ 1337. The K Weakest Rows in a Matrix/src/k_weakest_rows.ts
@@ -1,6 +1,21 @@
 function kWeakestRows(mat: number[][], k: number): number[] {
+  let ordmat: number[][] = [];
+  let result: number[] = [];
 
-  return [2,0,3];
+  for (let i = 0; i < mat.length; i++) {
+    ordmat.push([i, mat[i].reduce((p, c) => p + c, 0)]);
+  }
+  ordmat.sort( (a: number[], b: number[]): number => {
+    return a[0] - b[0]
+  });
+  ordmat.sort( (a: number[], b: number[]): number => {
+    return a[1] - b[1]
+  });
+  for (let i = 0; i < k; i++) {
+    result.push(ordmat[i][0]);
+  }
+  
+  return result;
 };
 
 export default kWeakestRows;
