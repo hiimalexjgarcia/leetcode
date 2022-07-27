8f383b567bf1d3a04e88f63dca7ad003aa0adeb4
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2022-07-27 01:36:41 -0700

    Solution 14: Added test for example 2.

--- 14. Longest Common Prefix/src/longest_common_prefix.test.ts
+++ 14. Longest Common Prefix/src/longest_common_prefix.test.ts
@@ -3,3 +3,7 @@ import longestCommonPrefix from './longest_common_prefix';
 test(' strs = ["flower","flow","flight"] => "fl" ', () => {
   expect( longestCommonPrefix( ["flower","flow","flight"] ) ).toBe("fl");
 })
+
+test(' strs = ["dog","racecar","car"] => "" ', () => {
+  expect( longestCommonPrefix( ["dog","racecar","car"] ) ).toBe("");
+})
--- 14. Longest Common Prefix/src/longest_common_prefix.ts
+++ 14. Longest Common Prefix/src/longest_common_prefix.ts
@@ -1,6 +1,20 @@
 function longestCommonPrefix(strs: string[]): string {
+  let result = "";
 
-  return "fl";
+  if (strs.length == 1) return strs[0];
+
+  const maxLength = Math.min( ...strs.map( (s: string) => s.length ) );
+  if (maxLength == 0) return "";
+
+  for (let i = 0; i < maxLength ; i++) {
+    const matches = strs.map( (s) => s[i] );
+    if ( matches.every( (c) => c == matches[0] )) {
+      result = result + matches[0].toString();
+    } else
+      break;
+  }
+
+  return result;
 };
 
 export default longestCommonPrefix;
