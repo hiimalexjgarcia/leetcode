ef952dae1981cbd59d44c939540dda4f167b9b2d
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2022-07-26 23:50:18 -0700

    Solution 1: Added test for example 3, added additional test.

--- 1. Two Sum/src/two_sum.test.ts
+++ 1. Two Sum/src/two_sum.test.ts
@@ -9,3 +9,13 @@ test(' nums = [3,2,4], target = 6 => [1, 2] ', () => {
 
   expect( twoSum( [3,2,4], 6 ) ).toEqual( expect.arrayContaining( [1, 2] ) );
 })
+
+test(' nums = [3,3], target = 6 => [0, 1] ', () => {
+
+  expect( twoSum( [3,3], 6 ) ).toEqual( expect.arrayContaining( [0, 1] ) );
+})
+
+test(' nums = [2,5,5,11], target = 10 => [1, 2] ', () => {
+
+  expect( twoSum( [2,5,5,11], 10 ) ).toEqual( expect.arrayContaining( [1, 2] ) );
+})
--- 1. Two Sum/src/two_sum.ts
+++ 1. Two Sum/src/two_sum.ts
@@ -1,8 +1,8 @@
 function twoSum(nums: number[], target: number): number[] {
-  let result: number[] = [];
+  let result = [];
 
   for (let a = 0; a < nums.length-1; a++) {
-    for (let b = 1; b < nums.length; b++) {
+    for (let b = a + 1; b < nums.length; b++) {
       if (nums[a] + nums[b] == target) {
         result.push(a);
         result.push(b);
