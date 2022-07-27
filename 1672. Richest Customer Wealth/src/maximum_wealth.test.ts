54439bbffea6dce17ae8aaaf23c94e7b587723ce
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2022-07-26 19:28:43 -0700

    Solution 1672: Added tests for examples 2 and 3.

--- 1672. Richest Customer Wealth/src/maximum_wealth.test.ts
+++ 1672. Richest Customer Wealth/src/maximum_wealth.test.ts
@@ -4,3 +4,13 @@ test('accounts = [[1,2,3], [3,2,1]] => 6', () => {
 
   expect( maximumWealth( [[1,2,3], [3,2,1]] ) ).toBe( 6 );
 })
+
+test('accounts = [[1,5],[7,3],[3,5]] => 10', () => {
+
+  expect( maximumWealth( [[1,5],[7,3],[3,5]] ) ).toBe( 10 );
+})
+
+test('accounts = [[2,8,7],[7,1,3],[1,9,5]] => 17', () => {
+
+  expect( maximumWealth( [[2,8,7],[7,1,3],[1,9,5]] ) ).toBe( 17 );
+})
--- 1672. Richest Customer Wealth/src/maximum_wealth.ts
+++ 1672. Richest Customer Wealth/src/maximum_wealth.ts
@@ -1,6 +1,12 @@
 function maximumWealth(accounts: number[][]): number {
+  let result = 0;
 
-  return 6;
+  accounts.forEach( (account) => {
+    let sum = account.reduce( (p, c) => p + c, 0);
+    if (sum > result) result = sum;
+  });
+
+  return result;
 };
 
 export default maximumWealth;
