5c809732778dbbfb72796c1a6055e72dc340eb2c
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2022-07-26 18:22:29 -0700

    Solution 1342: Added tests for examples 2 and 3.

--- 1342. Number of Steps to Reduce a Number to Zero/src/reduce_to_zero.test.ts
+++ 1342. Number of Steps to Reduce a Number to Zero/src/reduce_to_zero.test.ts
@@ -4,3 +4,13 @@ test('num = 14 => 6', () => {
 
   expect( numberOfSteps( 14 ) ).toBe( 6 );
 })
+
+test('num = 8 => 4', () => {
+
+  expect( numberOfSteps( 8 ) ).toBe( 4 );
+})
+
+test('num = 123 => 12', () => {
+
+  expect( numberOfSteps( 123 ) ).toBe( 12 );
+})
--- 1342. Number of Steps to Reduce a Number to Zero/src/reduce_to_zero.ts
+++ 1342. Number of Steps to Reduce a Number to Zero/src/reduce_to_zero.ts
@@ -1,6 +1,16 @@
 function numberOfSteps(num: number): number {
+  let result = 0;
 
-  return 6
+  while (num != 0) {
+    if (num % 2 == 0) {
+      num = num / 2;
+    } else {
+      num = num - 1;
+    }
+    result++;
+  }
+
+  return result;
 };
 
 export default numberOfSteps;
