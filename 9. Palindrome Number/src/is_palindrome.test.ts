2eebfb02b2f823c0ccd97196cc643ff6dfcd1992
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2022-07-29 00:24:02 -0700

    Solution 9: Added tests for examples 2 and 3.

--- 9. Palindrome Number/src/is_palindrome.test.ts
+++ 9. Palindrome Number/src/is_palindrome.test.ts
@@ -3,3 +3,11 @@ import isPalindrome from './is_palindrome'
 test('x = 121 => true', () => {
   expect( isPalindrome(121) ).toEqual(true);
 });
+
+test('x = -121 => false', () => {
+  expect( isPalindrome(-121) ).toEqual(false);
+});
+
+test('x = 10 => false', () => {
+  expect( isPalindrome(10) ).toEqual(false);
+});
--- 9. Palindrome Number/src/is_palindrome.ts
+++ 9. Palindrome Number/src/is_palindrome.ts
@@ -1,6 +1,8 @@
 function isPalindrome(x: number): boolean {
+  if (x.toString() !== x.toString().split("").reverse().join(""))
+    return false;
 
-  return true
+  return true;
 };
 
 export default isPalindrome;
