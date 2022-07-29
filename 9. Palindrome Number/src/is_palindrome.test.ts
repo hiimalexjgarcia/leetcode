b43517885b61d32f300d981ca58c7d3bc68686a0
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2022-07-29 00:17:36 -0700

    Solution 9: Initial commit, added test for example 1.

---
+++ 9. Palindrome Number/src/is_palindrome.test.ts
@@ -0,0 +1,5 @@
+import isPalindrome from './is_palindrome'
+
+test('x = 121 => true', () => {
+  expect( isPalindrome(121) ).toEqual(true);
+});
---
+++ 9. Palindrome Number/src/is_palindrome.ts
@@ -0,0 +1,6 @@
+function isPalindrome(x: number): boolean {
+
+  return true
+};
+
+export default isPalindrome;
